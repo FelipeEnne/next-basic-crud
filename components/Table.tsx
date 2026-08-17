177f7c97662f7e359b2697fefba66b6936353693
FelipeEnne <felipeenne@gmail.com>
2026-08-17 20:14:06 -0300

    feature: add style to table

--- components/Table.tsx
+++ components/Table.tsx
@@ -7,11 +7,11 @@ interface TableProps {
 export default function Table(props: TableProps) {
   function renderHeader() {
     return (
-      <thead>
+      <thead className="bg-linear-to-r from-purple-500 to-purple-800 text-gray-100">
         <tr>
-          <th>Code</th>
-          <th>Name</th>
-          <th>Age</th>
+          <th className="text-left p-4">Code</th>
+          <th className="text-left p-4">Name</th>
+          <th className="text-left p-4">Age</th>
         </tr>
       </thead>
     );
@@ -21,17 +21,20 @@ export default function Table(props: TableProps) {
     return (
       <tbody>
         {props.clients?.map((client, i) => (
-          <tr key={client.getId()}>
-            <td>{client.getId()}</td>
-            <td>{client.getName()}</td>
-            <td>{client.getAge()}</td>
+          <tr
+            key={`client-row-${i}-${client.getId()}`}
+            className={`${i % 2 === 0 ? "bg-purple-200" : "bg-purple-100"}`}
+          >
+            <td className="text-left p-4">{client.getId()}</td>
+            <td className="text-left p-4">{client.getName()}</td>
+            <td className="text-left p-4">{client.getAge()}</td>
           </tr>
         ))}
       </tbody>
     );
   }
   return (
-    <table>
+    <table className="w-full rounded-md overflow-hidden">
       {renderHeader()}
       {renderBody()}
     </table>
